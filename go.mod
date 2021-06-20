11e5a6d914e75db6b8d688af3621c05cb7e1b9a3
kedi hacker <8588261-kedihacker@users.noreply.gitlab.com>
2021-06-20 17:11:06 +0300

    hey

--- go.mod
+++ go.mod
@@ -2,4 +2,4 @@ module kedy
 
 go 1.16
 
-require github.com/google/btree v1.0.1 // indirect
+require github.com/google/btree v1.0.1
--- packages/bptree.go
+++ packages/bptree.go
@@ -5,27 +5,37 @@ type node struct {
 	child []*node
 }
 
-func (n *node) splt(i int) (Item, *node) {
-	item := n.items[i]
-	next := n.cow.newNode()
-	next.items = append(next.items, n.items[i+1:]...) //add items to the irght
-	n.items.truncate(i)
-	if len(n.children) > 0 {
-		next.children = append(next.children, n.children[i+1:]...)
-		n.children.truncate(i + 1)
-	}
-	return item, next
-}
+// func (n *node) splt(i int) (Item, *node) {
+// 	item := n.items[i]
+// 	next := n.cow.newNode()
+// 	next.items = append(next.items, n.items[i+1:]...) //add items to the irght
+// 	n.items.truncate(i)
+// 	if len(n.children) > 0 {
+// 		next.children = append(next.children, n.children[i+1:]...)
+// 		n.children.truncate(i + 1)
+// 	}
+// 	return item, next
+// }
 
-func (n *node) split() *node {
-	new := node{}                                             //new
-	split_index := len(n.items) / 2                           //set index
+func (n *node) split() (int, *node) {
+	new := node{} //new
+	split_index := len(n.items) / 2
+	item := n.items[split_index]                              //set index
 	new.items = append(new.items, n.items[split_index+1:]...) //copy items
 	new_array := make([]int, 5)                               //new array
-	copy(new_array, n.items[split_index+1:])                  //turnucatw array
-	if len(n.child) > 0 {                                     //if isnt leaf
+	copy(new_array, n.items[:split_index])                    //turnucatw array
+	n.items = new_array                                       //ASSİNG array
 
+	if len(n.child) > 0 { //if isnt leaf
+		new.child = append(new.child, n.child[split_index+1:]...) //copy items
+		new_array2 := make([]*node, 5)                            //new array
+		copy(new_array2, n.child[:split_index])                   //copy child
+		n.child = new_array2                                      //assing array
 	}
-	return &new
+	return item, &new
+
+}
+
+func (n *node) Insert(value int) {
 
 }
